50283f97b9d4496cb8c914f4cfeb7027c808a426
Salvador G <pulposh@gmail.com>
2019-01-08 11:15:17 +0100

    chapter 10

--- CppPrimer/CppPrimer/Chapter_10/Chapter10_Functions.h
+++ CppPrimer/CppPrimer/Chapter_10/Chapter10_Functions.h
@@ -13,6 +13,7 @@
 #include <forward_list>
 #include <algorithm>
 #include <stack>
+#include <functional>
 
 using std::cin;
 using std::cout;
@@ -37,3 +38,34 @@ void exer1020(vector<string> &words, vector<string>::size_type sz);
 
 // Exercise 10.21
 void exer1021();
+
+// Exercise 10.22
+void exer1022(vector<string> &words, vector<string>::size_type sz);
+bool check_size(const string &s, string::size_type sz);
+
+// Exercise 10.24
+void exer1024();
+
+// Exercise 10.25
+void exer1025(vector<string> &words, vector<string>::size_type sz);
+
+// Exercise 10.27
+void exer1027(vector<string> &words);
+
+// Exercise 10.28
+void exer1028();
+
+// Exercise 10.29
+void exer1029();
+
+// Exercise 10.30
+void exer1030();
+
+// Exercise 10.31
+void exer1031();
+
+// Exercise 10.32
+void exer1032(); // TODO
+
+// Exercise 10.33
+void exercise1033();
